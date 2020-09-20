4c781c6bb8ea64c560fb7f17fc65053c5598c1df
art-abdulwadud <abdulwadudhajike@gmail.com>
2020-09-20 18:21:00 +0300

    fix:fix gatsby failing to build static html for pages
    install gatsby-plugin-firebase
    add firebase imports in gatsby-browser.js and gatsby-ssr.js files
    add gatsby-plugin-firebase configurations in gatsby-config.js
    replace firebase imports in Auth.context.js and Home.conetext.js

--- admin/gatsby-browser.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import AuthProvider from './src/components/admin-login/Auth.context';
-
-export const wrapRootElement = ({ element }) => (
-    <AuthProvider>{element}</AuthProvider>
-);
